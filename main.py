2bd59ea6644327a56b11a96c996905867798cd60
lwz <905328134@qq.com>
2024-06-15 20:51:18 +0800

    initial

--- main.py
+++ main.py
@@ -1,16 +1,50 @@
-# This is a sample Python script.
+# main.py
+# ! pip install torchvision
+import torch, torch.nn as nn, torch.utils.data as data, torchvision as tv, torch.nn.functional as F
+import lightning as L
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# --------------------------------
+# Step 1: Define a LightningModule
+# --------------------------------
+# A LightningModule (nn.Module subclass) defines a full *system*
+# (ie: an LLM, diffusion model, autoencoder, or simple image classifier).
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+class LitAutoEncoder(L.LightningModule):
+    def __init__(self):
+        super().__init__()
+        self.encoder = nn.Sequential(nn.Linear(28 * 28, 128), nn.ReLU(), nn.Linear(128, 3))
+        self.decoder = nn.Sequential(nn.Linear(3, 128), nn.ReLU(), nn.Linear(128, 28 * 28))
+
+    def forward(self, x):
+        # in lightning, forward defines the prediction/inference actions
+        embedding = self.encoder(x)
+        return embedding
+
+    def training_step(self, batch, batch_idx):
+        # training_step defines the train loop. It is independent of forward
+        x, y = batch
+        x = x.view(x.size(0), -1)
+        z = self.encoder(x)
+        x_hat = self.decoder(z)
+        loss = F.mse_loss(x_hat, x)
+        self.log("train_loss", loss)
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        return optimizer
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# -------------------
+# Step 2: Define data
+# -------------------
+dataset = tv.datasets.MNIST("/home/ma-user/work/wenzhuoliu", download=True, transform=tv.transforms.ToTensor())
+train, val = data.random_split(dataset, [55000, 5000])
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# -------------------
+# Step 3: Train
+# -------------------
+autoencoder = LitAutoEncoder()
+trainer = L.Trainer(accelerator='npu', devices='0,1', max_epochs=1, strategy='deepspeed')
+trainer.fit(autoencoder, data.DataLoader(train), data.DataLoader(val))
