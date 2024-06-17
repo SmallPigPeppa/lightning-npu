6930e1aaf186cb63bba2a42477799f80a411bbcb
lwz <905328134@qq.com>
2024-06-17 16:27:01 +0800

    initial

--- cifar100_224x224.py
+++ cifar100_224x224.py
@@ -91,7 +91,7 @@ class ResNet50Classifier(pl.LightningModule):
 
 
 def main():
-    data_module = CIFAR100DataModule(batch_size=128)
+    data_module = CIFAR100DataModule(batch_size=256)
     model = ResNet50Classifier()
     trainer = Trainer(accelerator='npu', devices='0,1', max_epochs=5, precision=16)
     trainer.fit(model, datamodule=data_module)
