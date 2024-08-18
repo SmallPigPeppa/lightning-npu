5525c9e19cedaa1d7bb9bf8085a1a83f95dad8fd
lwz <905328134@qq.com>
2024-08-18 19:54:49 +0800

    initial

--- debug.py
+++ debug.py
@@ -1,7 +1,7 @@
 # from pytorch_lightning.loggers import LightningLoggerBase, TensorBoardLogger, WandbLogger
 
 
-from pl_bolts.optimizers.lr_scheduler import LinearWarmupCosineAnnealingLR
+# from pl_bolts.optimizers.lr_scheduler import LinearWarmupCosineAnnealingLR
 import timm
 
 model = timm.create_model('resnet50', pretrained=True)
