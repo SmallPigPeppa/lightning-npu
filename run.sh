8e02538301a41989d317ca713324cae4ee295d7e
lwz <905328134@qq.com>
2024-08-19 21:30:12 +0800

    initial

---
+++ run.sh
@@ -0,0 +1,7 @@
+export no_proxy='huggingface.co'
+pip install /opt/huawei/dataset/all/code/clip-lightning/requirements-hr.txt
+wandb login 7b6d07bce88338250492397fd23e6cd84e8efdd2
+cp -r /opt/huawei/dataset/all/huggingface ~/.cache/
+cd /opt/huawei/dataset/all/code/lightning-npu
+git pull origin
+python cifar100_224x224_fp16.py
