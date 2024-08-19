7a3363516606949881d2fbb9cff2c9a613970910
lwz <905328134@qq.com>
2024-08-19 22:11:42 +0800

    initial

---
+++ run.sh
@@ -0,0 +1,21 @@
+# Set environment variable to bypass proxy for specific domains
+export no_proxy='huggingface.co'
+
+# Install necessary libraries from specified directories
+pip install /opt/huawei/dataset/all/code/pytorch-lightning torch==2.1.0
+pip install -r /opt/huawei/dataset/all/code/clip-lightning/requirements-hr.txt
+
+# Log in to Weights & Biases (wandb) with the provided token
+wandb login 7b6d07bce88338250492397fd23e6cd84e8efdd2
+
+# Copy datasets or models to the local cache directory
+cp -r /opt/huawei/dataset/all/huggingface ~/.cache/
+
+# Navigate to the working directory where the code is located
+cd /opt/huawei/dataset/all/code/lightning-npu
+
+# Update the repository to the latest version from the origin
+git pull origin
+
+# Execute the python script for training on CIFAR100 with specific configurations
+python cifar100_224x224_fp16.py
