99f7b5813832abcb1a1651045cb3297beedc2b56
lwz <905328134@qq.com>
2024-08-19 21:44:27 +0800

    initial

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 export no_proxy='huggingface.co'
 pip install /opt/huawei/dataset/all/code/pytorch-lightning torch==2.1.0
-pip install /opt/huawei/dataset/all/code/clip-lightning/requirements-hr.txt
+pip install -r /opt/huawei/dataset/all/code/clip-lightning/requirements-hr.txt
 wandb login 7b6d07bce88338250492397fd23e6cd84e8efdd2
 cp -r /opt/huawei/dataset/all/huggingface ~/.cache/
 cd /opt/huawei/dataset/all/code/lightning-npu
