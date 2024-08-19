ce2e27901fed6cf087e9fc4f9176797c6766c164
lwz <905328134@qq.com>
2024-08-19 21:43:52 +0800

    initial

--- run_debug.sh
+++ run_debug.sh
@@ -1,5 +1,4 @@
 export no_proxy='huggingface.co'
-pip install /opt/huawei/dataset/all/code/pytorch-lightning torch==2.1.0
 pip install /opt/huawei/dataset/all/code/clip-lightning/requirements-hr.txt
 wandb login 7b6d07bce88338250492397fd23e6cd84e8efdd2
 cp -r /opt/huawei/dataset/all/huggingface ~/.cache/
