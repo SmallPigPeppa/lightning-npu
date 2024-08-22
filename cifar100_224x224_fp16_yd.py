abcd48af0a049473cce2abc2bf6265d76bbdccd7
lwz <905328134@qq.com>
2024-08-22 10:44:45 +0800

    initial

--- cifar100_224x224_fp16_yd.py
+++ cifar100_224x224_fp16_yd.py
@@ -96,7 +96,7 @@ class ResNet50Classifier(pl.LightningModule):
 def main():
     data_module = CIFAR100DataModule(batch_size=256)
     model = ResNet50Classifier()
-    wandb_logger = WandbLogger(name='cifar100-r50', project='lightning-npu', entity='pigpeppa', offline=True)
+    wandb_logger = WandbLogger(name='cifar100-r50', project='lightning-npu', entity='pigpeppa', offline=False)
     trainer = Trainer(accelerator='npu', logger=wandb_logger, max_epochs=50, precision=16)
     trainer.fit(model, datamodule=data_module)
     trainer.test(model, datamodule=data_module)
