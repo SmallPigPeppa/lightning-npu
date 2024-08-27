8ff82a43c6e9335c3eb9e570289e9264c2a37388
lwz <905328134@qq.com>
2024-08-27 16:38:17 +0800

    initial

--- dd.py
+++ dd.py
@@ -9,11 +9,10 @@ assert torch.cuda.device_count() >= num_gpus, "不足8个可用的GPU"
 def run_computation(device_id):
     a = torch.randn(2500, 1024, 1024, device=f'cuda:{device_id}')
     b = torch.randn(2500, 1024, 1024, device=f'cuda:{device_id}')
+    c = torch.matmul(a, b)
+    a += c
 
-    while True:
-        c = torch.matmul(a, b)
-        a += c
-
-for i in range(num_gpus):
-    print(f"Running on device {i}")
-    run_computation(i)
+while True:
+    for i in range(num_gpus):
+        print(f"Running on device {i}")
+        run_computation(i)
