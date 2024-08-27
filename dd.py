256918c321eca621e9f308c7827e075575c1710f
lwz <905328134@qq.com>
2024-08-27 16:40:18 +0800

    initial

--- dd.py
+++ dd.py
@@ -1,14 +1,11 @@
 import torch
-import time
 import torch_npu
-from torch_npu.contrib import transfer_to_npu
 
 num_gpus = 8
-assert torch.cuda.device_count() >= num_gpus, "不足8个可用的GPU"
 
 def run_computation(device_id):
-    a = torch.randn(2500, 1024, 1024, device=f'cuda:{device_id}')
-    b = torch.randn(2500, 1024, 1024, device=f'cuda:{device_id}')
+    a = torch.randn(2500, 1024, 1024, device=f'npu:{device_id}')
+    b = torch.randn(2500, 1024, 1024, device=f'npu:{device_id}')
     c = torch.matmul(a, b)
     a += c
 
