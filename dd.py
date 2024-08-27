ace2476509a54a51dac159bba18bca05b5d5b55f
lwz <905328134@qq.com>
2024-08-27 16:56:28 +0800

    initial

--- dd.py
+++ dd.py
@@ -3,10 +3,12 @@ import torch_npu
 
 num_gpus = 8
 
+
 def run_computation(device_id):
     a = torch.randn(2500, 1024, 1024, device=f'npu:{device_id}')
     b = torch.randn(2500, 1024, 1024, device=f'npu:{device_id}')
-    c = torch.matmul(a, b)
+    c = a * b
+
 
 while True:
     for i in range(num_gpus):
