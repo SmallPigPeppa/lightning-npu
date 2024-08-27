5771355c394252332e3bdda220690f5f11664177
lwz <905328134@qq.com>
2024-08-27 13:25:21 +0800

    initial

--- dddd_backup.py
+++ dddd_backup.py
@@ -9,7 +9,7 @@ assert torch.cuda.device_count() >= num_gpus, "不足8个可用的GPU"
 
 
 def run_computation(device_id):
-    torch.cuda.set_device(device_id)
+    # torch.cuda.set_device(device_id)
     a = torch.randn(2500, 1024, 1024, device=f'cuda:{device_id}')
     b = torch.randn(2500, 1024, 1024, device=f'cuda:{device_id}')
 
